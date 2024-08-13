68d309f825428ba94f8a5da5089e8fbf7eea418a
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-08-13 10:45:53 -0400

    server setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`)
+})
