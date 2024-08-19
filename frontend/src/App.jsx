1caab6bb6d355c89c18af416d5480a188cce3415
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-08-19 08:14:08 -0400

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
