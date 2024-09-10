1a16801fce711827f252ad62e412c6fea755295a
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-09-10 00:56:55 -0400

    deployment

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://foodie-frenzy-tsws.onrender.com"
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
